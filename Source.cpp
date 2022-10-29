3a50812058427ddf6214de3edecc95e92c7c1be3
Larkin <ab88199594ae@mail.ru>
2022-10-29 19:27:59 +0300

    create list
    Создан односвязный список
    Создан класс работы с файлами

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,5 @@
 // Source.cpp : Defines the entry point for the console application.
+//
 
 #include "stdafx.h"
 #include <conio.h>
@@ -6,33 +7,46 @@
 #include <locale.h>
 
 // Объявление глобальных переменных
-struct PRODUKT
-{
-	char name[50];
-	char naz[10];
+
+class KOR
+{public:
+
+	
+
+	char Name[50];
+	char Nazv[10];
 	int kol;
 	float cena;
+	KOR *next;
+
+
+	void AddItem();
+	void LoadTxtFile();
+	void SaveTxtFile();
+	void removeAt();
+	void DeleteList(KOR*);
+	int GetIndex();
+	void SortList();
+	void EditItem();
+	KOR* CreateItem(const char* Name, const char* Nazv, int kol, float cena);
+	KOR* GetTail(KOR*);
+	KOR* push_back(KOR*, KOR*);
+	
+	
 };
-PRODUKT st[50];
+KOR *head=NULL;
 int numrec=0;
 
 // Объявление функций
-int  ShowMenu();
-void AddItem();
-void EditItem();
-void DeleteItem();
-void PrintPRODUKT(int numrec);
-void LoadTxtFile();
-void LoadBinFile();
-void SaveTxtFile();
-void SaveBinFile();
-void SaveReport();
-int  GetIndex();
-void SortList();
+int ShowMenu();
+void PrintKor(KOR*, int);
 
+// Главная функция
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int nMenu;
+	int nMenu,i;
+	KOR *st;
+	KOR a;
 
 	setlocale(LC_CTYPE,"Russian");
 	do
@@ -42,61 +56,67 @@ int _tmain(int argc, _TCHAR* argv[])
 		{
 		case 1:
 			  {
-				  AddItem();
+				  a.AddItem();
 				  break;
 			  }
 		case 2:
 			  {
-				  EditItem();
+				  a.EditItem();
 				  break;
 			  }
 		case 3:
 			  {
-				  DeleteItem();
+				  a.removeAt();
 				  break;
 			  }
 		case 4:
-			  {
-				  SortList();
-				  if(numrec==0)
+			  {if(!head)
 				  {
 					  printf("\nБаза данных пуста...\n");
 					  break;
 				  }
+				  a.SortList();
+				  
 				  printf("\n");
 				  printf("+----+--------------+--------------------+----------------+--------+\n");
 				  printf("| №  |    Автор     |      Название      | Кол-во страниц |  Цена  |\n");
 	              printf("+----+--------------+--------------------+----------------+--------+\n");
-				  for(int i=0;i<numrec;++i)
+				  st = head;
+				  i=1;
+				  while(st)
 				  {
-					  PrintPRODUKT(i);
+					  PrintKor(st,i++);
+					  st=st->next;
 				  }
 				  printf("+----+--------------+--------------------+----------------+--------+\n");
 				  break;
 			  }
 		case 5:
 			  {
-				  LoadTxtFile();
+				  if(head)
+				  {
+					  a.DeleteList(head);
+					  head=NULL;
+					  numrec=0;
+				  }
+				  a.LoadTxtFile();
 				  break;
 			  }
 		case 6:
 			  {
-				  LoadBinFile();
+				  a.SaveTxtFile();
 				  break;
 			  }
 		case 7:
 			  {
-				  SaveTxtFile();
-				  break;
-			  }
-		case 8:
-			  {
-				  SaveBinFile();
-				  break;
-			  }
-		case 9:
-			  {
-				  SaveReport();
+				  if(!head)
+				  {
+					  printf("\nБаза данных и так пуста!\n"); 
+					  break;
+				  }
+				  a.DeleteList(head);
+				  head=NULL;
+				  numrec=0;
 				  break;
 			  }
 		default: printf("\nНеправильный пункт меню...\n");
@@ -110,16 +130,14 @@ int ShowMenu()
 {
 	int choice;
 
-	printf("\n===============================================================================\n");
+	printf("\n====================================================================\n");
 	printf("1 - Добавить запись\n");
 	printf("2 - Изменить запись\n");
 	printf("3 - Удалить  запись\n");
 	printf("4 - Показать весь список\n");
 	printf("5 - Загрузить текстовый файл\n");
-	printf("6 - Загрузить двойчный файл\n");
-	printf("7 - Сохранить текстовый файл\n");
-	printf("8 - Сохранить двоичный файл\n");
-	printf("9 - Сохранить отчет\n");
+	printf("6 - Сохранить текстовый файл\n");
+	printf("7 - Удалить список\n");
 	printf("0 - Выход\n");
 	printf("Количество записей в базе: %d\n",numrec);
 	printf("Ваш выбор: ");
@@ -127,72 +145,132 @@ int ShowMenu()
 	return choice;
 }
 
-// Функция ввода данных о продукте
-void AddItem()
+// Функция ввода данных о студенте
+void KOR :: AddItem()
 {
-	flushall();
-	printf("Автор : ");
-	gets(st[numrec].name); 
-	printf("Название   : ");
-	gets(st[numrec].naz);
-	printf("Кол-во страниц: ");
-	scanf("%d",&st[numrec].kol);
-	printf("Цена: ");
-	scanf("%f",&st[numrec].cena);
+	char Name[50];
+	char Nomer[10];
+	int mest;
+	float cena;
+	KOR *st, *tail,a;
+
+	flushall();			
+	printf("Автор          : ");
+	gets(Name); 
+	printf("Название       : ");
+	gets(Nazv);
+	printf("Кол-во страниц : ");
+	scanf("%d",&kol);
+	printf("Цена           : ");
+	scanf("%f",&cena);
+
+	tail = GetTail(head);
+	st=a.push_back(tail, a.CreateItem(Name, Nazv, kol, cena));
+	if (!head)
+		head = st;
 	++numrec;
 }
 
 // Функция изменения записи
-void EditItem()
+void KOR :: EditItem()
 {
 	int index;
+	KOR *st,a;
 
-	index=GetIndex();
+	index=a.GetIndex();
 	if(index==-1)
 		return;
+	st=head;
+	for(int i=1;i<index;++i)
+		st=st->next;
 	printf("Сейчас эта запись содержит следующие данные:\n");
 	printf("\n");
 	printf("+----+--------------+--------------------+----------------+--------+\n");
 	printf("| №  |    Автор     |      Название      | Кол-во страниц |  Цена  |\n");
 	printf("+----+--------------+--------------------+----------------+--------+\n");
-	PrintPRODUKT(index);
+	PrintKor(st, index);
 	printf("+----+--------------+--------------------+----------------+--------+\n");
 	printf("\nВведите новые данные:\n");
 	flushall();
 	printf("Автор : ");
-	gets(st[index].name); 
-	printf("Название   : ");
-	gets(st[index].naz);
+	gets(st->Name); 
+	printf("Название : ");
+	gets(st->Nazv);
 	printf("Кол-во страниц: ");
-	scanf("%d",&st[index].kol);
+	scanf("%d",&st->kol);
 	printf("Цена: ");
-	scanf("%f",&st[index].cena);
+	scanf("%f",&st->cena);
 	printf("\nДанные изменены.\n");
 }
 
 // Функция удаления записи
-void DeleteItem()
+void KOR :: removeAt()
 {
 	int index;
+	KOR *st,a;
 
-	index=GetIndex();
+	index=a.GetIndex();
 	if(index==-1)
 		return;
-	for(int i=index;i<numrec-1;++i)
-		st[i]=st[i+1];
+	if(index==1)
+	{
+		head=head->next;
+		--numrec;
+		return;
+	}
+	st=head;
+	for(int i=1;i<index-1;++i)
+		st=st->next;
+	st->next=st->next->next;
 	--numrec;
 }
 
-// Функция вывода на экран информации об одном продукте
-void PrintPRODUKT(int i)
+// НОВАЯ Функция создает новый элемент (пока вне списка)
+KOR* KOR :: CreateItem(const char* Name, const char* Nazv, int kol, float cena)
 {
-	printf("| %-2d | %-12s | %-18s | %-14d | %-6.1f |\n",i+1,st[i].name,st[i].naz,st[i].kol,st[i].cena);
+	KOR *st = new KOR;
+	strcpy(st->Name, Name);
+	strcpy(st->Nazv, Nazv);
+	st->kol=kol;
+	st->cena=cena;
+	st->next=NULL;
+	return st;
+}
+
+// НОВАЯ Функция находит последний элемент списка
+KOR* KOR :: GetTail(KOR* st)
+{
+	while(st && st->next)
+		st=st->next;
+	return st;
+}
+
+// Функция добавляет созданный элемент в конец списка
+KOR* KOR :: push_back(KOR *parent, KOR *st)
+{
+	if (parent) 
+	{
+		st->next = parent->next;
+		parent->next = st;
+	}
+	return st;
+}
+
+// Функция печатает указанный элемент списка
+void PrintKor(KOR *st, int i)
+{
+	printf("| %-2d | %-12s | %-18s | %-14d | %-6.1f |\n",i,st->Name,st->Nazv,st->kol,st->cena);
 }
 
 // Функция чтения БД из текстового файла
-void LoadTxtFile()
+void KOR :: LoadTxtFile()
 {
 	FILE *fp;
+	char Name[50];
+	char Nazv[10];
+	int kol;
+	float cena;
+	KOR *st, *tail,a;
 
 	fp=fopen("db.txt","r");
 	if(fp==NULL)
@@ -200,45 +278,36 @@ void LoadTxtFile()
 		printf("\nОшибка открытия файла...\n");
 		return;
 	}
+	// здесь надо удалить список, если он существует
 	fscanf(fp,"%d\n",&numrec);
 	for(int i=0;i<numrec;++i)
 	{
-		fgets(st[i].name,50,fp);
-		st[i].name[strlen(st[i].name)-1]='\0';
-		fgets(st[i].naz,10,fp);
-		st[i].naz[strlen(st[i].naz)-1]='\0';
-		fscanf(fp,"%d",&st[i].kol);
-		fscanf(fp,"%f\n",&st[i].cena);
-	}
-	printf("\nФайл прочитан.\n");
-	fclose(fp);
-}
-
-// Функция чтения БД из двоичного (бинарного) файла
-void LoadBinFile()
-{
-	FILE *fp;
-
-	fp=fopen("db.bin","r");
-	if(fp==NULL)
-	{
-		printf("\nОшибка открытия файла...\n");
-		return;
-	}
-	fread(&numrec,sizeof(int),1,fp);
-	for(int i=0;i<numrec;++i)
-	{
-		fread(&st[i],sizeof(PRODUKT),1,fp);
+		fgets(Name,50,fp);
+		Name[strlen(Name)-1]='\0';
+		fgets(Nazv,10,fp);
+		Nazv[strlen(Nazv)-1]='\0';
+		fscanf(fp,"%d\n",&kol);
+		fscanf(fp,"%f\n",&cena);
+		tail = a.GetTail(head);
+		st=a.push_back(tail, a.CreateItem(Name, Nazv, kol, cena));
+		if (!head)
+			head = st;
 	}
 	printf("\nФайл прочитан.\n");
 	fclose(fp);
 }
 
 // Функция сохранения БД в текстовый файл
-void SaveTxtFile()
+void KOR :: SaveTxtFile()
 {
 	FILE *fp;
+	KOR *st;
 
+	if(!head)
+	{
+		printf("\nБаза данных пуста...\n");
+		return;
+	}
 	fp=fopen("db.txt","w");
 	if(fp==NULL)
 	{
@@ -246,67 +315,35 @@ void SaveTxtFile()
 		return;
 	}
 	fprintf(fp,"%d\n",numrec);
-	for(int i=0;i<numrec;++i)
+	st=head;
+	while(st)
 	{
-		fprintf(fp,"%s\n",st[i].name);
-		fprintf(fp,"%s\n",st[i].naz);
-		fprintf(fp,"%d\n",st[i].kol);
-		fprintf(fp,"%f\n",st[i].cena);
+		fprintf(fp,"%s\n",st->Name);
+		fprintf(fp,"%s\n",st->Nazv);
+		fprintf(fp,"%d\n",st->kol);
+		fprintf(fp,"%f\n",st->cena);
+		st=st->next;
 	}
 	printf("\nФайл сохранен.\n");
 	fclose(fp);
 }
 
-// Функция сохранения БД в двоичный (бинарный) файл
-void SaveBinFile()
+// НОВАЯ Функция удаляет весь список
+void KOR :: DeleteList(KOR *head)
 {
-	FILE *fp;
-
-	fp=fopen("db.bin","w");
-	if(fp==NULL)
-	{
-		printf("\nОшибка открытия файла...\n");
-		return;
-	}
-	fwrite(&numrec,sizeof(int),1,fp);
-	for(int i=0;i<numrec;++i)
-	{
-		fwrite(&st[i],sizeof(PRODUKT),1,fp);
-	}
-	printf("\nФайл сохранен.\n");
-	fclose(fp);
-}
-
-// Функция сохранения БД в файл-отчет
-void SaveReport()
-{
-	FILE *fp;
-
-	fp=fopen("report.txt","w");
-	if(fp==NULL)
-	{
-		printf("\nОшибка открытия файла...\n");
-		return;
-	}
-	fprintf(fp,"==================================== ОТЧЕТ ====================================\n");
-	fprintf(fp,"+----+--------------+--------------------+----------------+--------+\n");
-	fprintf(fp,"| №  |    Автор     |      Название      | Кол-во страниц |  Цена  |\n");
-	fprintf(fp,"+----+--------------+--------------------+----------------+--------+\n");
-	for(int i=0;i<numrec;++i)
-	{
-		fprintf(fp,"| %-2d | %-30s | %-11s | %-12d | %-8.1f |\n",i+1,st[i].name,st[i].naz,st[i].kol,st[i].cena);
-	}
-	fprintf(fp,"+----+--------------+--------------------+----------------+--------+\n");
-	printf("\nОтчет сохранен.\n");
-	fclose(fp);
+	KOR *st,a;
+	st=head;
+	if (st->next)				//Пока есть еще элементы
+		a.DeleteList(st->next);	//удалить их
+	delete st;					//удалить текущий элемент
 }
 
 // Функция запроса номера записи
-int GetIndex()
+int KOR :: GetIndex()
 {
 	int index;
 
-	if(numrec==0)
+	if(!head)
 	{
 		printf("\nБаза данных пуста...\n");
 		return -1;
@@ -318,27 +355,44 @@ int GetIndex()
 		printf("\nНет записи с таким номером...\n");
 		return -1;
 	}
-	return index-1;
+	return index;
 }
-
-// Функция сортировки записей по полю Наименование
-void SortList()
+void KOR :: SortList()
 {
-	PRODUKT temp;
+	KOR *st, *stnext, temp;
+	
 	int sortOk;
-
 	do
 	{
 		sortOk=1;
-		for(int i=0;i<numrec-1;++i)
+		st=head;
+		stnext=head->next;
+		while(stnext)
 		{
-			if(strcmp(st[i].name,st[i+1].name)>0)
-			{
-				temp=st[i];
-				st[i]=st[i+1];
-				st[i+1]=temp;
-				sortOk=0;
-			}
+			if(strcmp(st->Name,stnext->Name)>0)
+				{
+
+					strcpy (temp.Name,st->Name);
+					strcpy (temp.Nazv,st->Nazv);
+					temp.kol=st->kol;
+					temp.cena=st->cena;
+
+					strcpy (st->Name,stnext->Name);
+					strcpy (st->Nazv,stnext->Nazv);
+					st->kol=stnext->kol;
+					st->cena=stnext->cena;
+
+					strcpy (stnext->Name,temp.Name);
+					strcpy (stnext->Nazv,temp.Nazv);
+					stnext->kol=temp.kol;
+					stnext->cena=temp.cena;
+
+						sortOk=0;
+				}
+			st=st->next;
+			stnext=stnext->next;
 		}
+	
 	}while(sortOk==0);
 }
+

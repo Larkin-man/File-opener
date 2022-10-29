376ef11bb00a5b6a3a1c520c53af6d9dfa715f3b
Larkin <ab88199594ae@mail.ru>
2022-10-29 20:20:10 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,18 +1,19 @@
-//*****КУРСОВАЯ РАБОТА ПО ДИСЦИПЛИНЕ*****//
-//*****"ТЕХНОЛОГИЯ ПРОГРАММИРОВАНИЯ"*****//
-//********Апонасенко А.В. КИ08-04********//
-
-#include<conio.h>
-#include<stdio.h>
+//---------------------------------------------------------------------------
+#include <stdio.h>
+#include <conio.h> //для getch
 #include<math.h>
 #include<string.h>
+#pragma hdrstop
+
+#include <tchar.h>
+//---------------------------------------------------------------------------
 
 struct Node     //Двусвязный список
 {
    char name[80];
-   int  num1;
+   char  symb;
    int  num2;
-   int  num3;
+   double  num3;
    Node *pNext;
    Node *pPrev;
 };
@@ -23,7 +24,7 @@ int nRecord;     //Количество списков
 Node *CreateItem (int id, const char *name, float mark);
 Node *InsertAfter (Node *parent, Node *Item);
 Node *getTail (Node *head);
-Node *PushBack (Node *head, Node *Item); 
+Node *PushBack (Node *head, Node *Item);
 void PrintNode (Node *Item);
 void PrintList (Node *head);
 void DeleteList (Node *Item);
@@ -37,13 +38,16 @@ void modifyItem();
 bool saveAsBinary();
 bool loadFromBinary();
 bool saveAsText();
-bool loadFromText(); 
+bool loadFromText();
 int getIndex();
 bool saveReport();
-
-void main()   //int _tmain(int argc, _TCHAR* argv[])
+bool ModifyBinary();
+//-------------------------------------------------------------------
+#pragma argsused
+int _tmain(int argc, _TCHAR* argv[])
 {
-	int nMenu = 0;
+int nMenu = 0;
+   printf ( "1.string 2.char 3.int 4.double.\n");
 
 	while ((nMenu = showMenu()) != 0)
 	{
@@ -76,20 +80,24 @@ void main()   //int _tmain(int argc, _TCHAR* argv[])
 		case 6:
 			if (loadFromBinary())
 			{
-				printf ( "File loaded.");
+				printf ( "File loaded. %d",nRecord);
 			}
 			break;
 		case 8:
 			if (loadFromText())
 			{
-				printf ( "File loaded.");
+				printf ( "File loaded. %d",nRecord);
 			}
 			break;
 		case 9:
-			if (saveReport())
+			if (ModifyBinary())
 			{
 				printf ( "Report saved.");
 			}
+			break;
+		case -5:
+			if (ModifyBinary())
+				printf ("Succes modify binary.");
 			break;
 		default:
 			printf ( "Wrong menu item! \n");
@@ -100,59 +108,65 @@ void main()   //int _tmain(int argc, _TCHAR* argv[])
 	{
 		delete [] Record;
 	}
-        
+
 	//getch(); //return 0;
 }
-
-Node *CreateItem (const char *name, int  num1, int  num2 ,int  num3)
+//---------------------------------------------------------------------------
+//-------------------------------------------------------------------
+Node *CreateItem (const char *name, char  symb, int  num2 ,double  num3)
 {                //Функция создает новый элемент
    Node *pItem = new Node;
-   strncpy(pItem->name,name,80);
-   pItem->num1=num1;
-   pItem->num2=num2;
-   pItem->num3=num3;
-   pItem->pNext=NULL;
-   pItem->pPrev=NULL;
+   strncpy(pItem->name, name, 80);
+   pItem->symb = symb;
+   pItem->num2 = num2;
+   pItem->num3 = num3;
+   pItem->pNext = NULL;
+   pItem->pPrev = NULL;
    return pItem;
 }
-
+//-------------------------------------------------------------------
 Node *InsertAfter (Node *parent, Node *Item)
 {                 //Функция связывает два элемента
    if (parent != NULL)
    {
       //Item->pNext=parent->pNext;
-      parent->pNext=Item;
-      Item->pPrev=parent;
+      parent->pNext = Item;
+      Item->pPrev = parent;
    }
    return Item;
 }
-
+//-------------------------------------------------------------------
 Node *getTail (Node *head)
 {             //Функция поиска последного элемента ы списке
    if(head == NULL)
       return NULL;
-   Node *Item=head;
+   Node *Item = head;
    while(Item->pNext != NULL)
       Item = Item->pNext;
    return Item;
 }
-
+//-------------------------------------------------------------------
 Node *PushBack (Node *head, Node *Item)
 {              //Функция вставляет элемент в конец списка
    Node *tail = getTail(head);
    return InsertAfter (tail, Item);
 }
-
+//-------------------------------------------------------------------
 void PrintNode (Node *Item)
 {              //Функция печатает один элемент на экране
    printf("%s ",Item->name);
-   printf("%d ",Item->num1);
+   printf("%c ",Item->symb);
    printf("%d ",Item->num2);
-   printf("%d \n",Item->num3);
+   printf("%f \n",Item->num3);
 }
-
+//-------------------------------------------------------------------
 void PrintList (Node *head)
 {              //Функция печатает весь список на экране
+   if (  nRecord == 0 )
+   {
+      printf("List is empty.\n");
+      return;
+   }
    Node *Item = head;
    printf("Record:\n");
    while(Item != NULL)
@@ -160,8 +174,8 @@ void PrintList (Node *head)
       PrintNode(Item);
       Item = Item->pNext;
    }
-}                              
-
+}
+//-------------------------------------------------------------------
 void DeleteList (Node *Item)
 {               //Функция удаляет список
    if (Item->pNext != NULL)
@@ -170,7 +184,7 @@ void DeleteList (Node *Item)
    }
    delete Item;
 }
-
+//-------------------------------------------------------------------
 Node *RemoveNode (Node *prev, Node *Item)
 {                //Функция удаляет элементы списка расположеные перед Item
    if (prev == Item)
@@ -187,7 +201,7 @@ Node *RemoveNode (Node *prev, Node *Item)
    }
    return Item;
 }
-
+//-------------------------------------------------------------------
 int showMenu()    //Функция показывает меню
 {
 	printf ( "\n1. Add	\n");
@@ -198,56 +212,55 @@ int showMenu()    //Функция показывает меню
 	printf ( "6. Load from Binary	\n");
 	printf ( "7. Save to Text		\n");
 	printf ( "8. Load from Text		\n");
-        printf ( "9. Save Report		\n");
+	printf ( "9. Save Report		\n");
+	printf ( "-5. Modify Binary		\n");
 	//printf ( "---------	\n");
 	printf ( "0. Exit	\n");
-	printf ( "\n?>");
+	printf ( "\n>:");
 
 	int choise = 0;
 	//cin >> choise;
-     scanf ("%d",&choise);
-
+   scanf ("%d",&choise);
 	return choise;
 }
-
+//-------------------------------------------------------------------
 void addItem()     //Функция добавляет элемент в список
 {
    printf ( "Please type a new value: ");
    char name[80];
-   int num1;
+   int symb;
    int num2;
-   int num3;
+   double num3;
    nRecord++;
-   scanf("%s %d %d %d",&name,&num1,&num2,&num3);
-   Node *Item = PushBack(Record,CreateItem(name,num1,num2,num3));
+   scanf("%s %c %d %le",&name,&symb,&num2,&num3);
+   Node *Item = PushBack(Record,CreateItem(name,symb,num2,num3));
    if(Record == NULL)
       Record=Item;
-
 }
-
+//-------------------------------------------------------------------
 void deleteItem()    //Функция удаляет выбраный элемент
 {
    Node *Item = Record;
    int nIdx = getIndex();
    for (int i=0;i<nIdx;i++)
       {
-      Item=Item->pNext;
+         Item=Item->pNext;
       }
-   
+
    if (Item->pNext == NULL)
        Item->pPrev->pNext=NULL;
    else
    if (Item->pPrev == NULL)
        {
-       Record=Record->pNext;
-       Item->pNext->pPrev=NULL;
+         Record=Record->pNext;
+         Item->pNext->pPrev=NULL;
        }
    else
-   InsertAfter(Item->pPrev,Item->pNext);
+      InsertAfter(Item->pPrev,Item->pNext);
    nRecord--;
 
 }
-
+//-------------------------------------------------------------------
 int getIndex()    //Функция возвращает индекс элемента
 {
 	if (Record == NULL)
@@ -256,71 +269,79 @@ int getIndex()    //Функция возвращает индекс элемента
 		return -1;
 	}
 	int nIdx = -1;
-	printf ( "Please enter an item's index to delete from array 1.. %d ",nRecord);
+	printf ( "Please enter an item's index to delete from array 1.. %d >:",nRecord);
 	//cin >> nIdx;
-     scanf("%d",&nIdx);
+   scanf("%d",&nIdx);
 
 	if (nIdx < 1 || nIdx > nRecord)
 	{
 		printf ( "Error: index is out of bound.");
 		return -1;
-	}
-
+   }
 	return nIdx - 1;
-
 }
+//-------------------------------------------------------------------
 void modifyItem()    //Функция изменяет элемент
 {
    int nIdx = getIndex();
    if (nIdx == -1)
    {
-   return;
+      return;
    }
 
-   //printf ( "You are going to replace value " << Record->name << " at position " << nIdx + 1 << "." \n");
+   //cout ( "You are going to replace value " << Record->name << " at position " << nIdx + 1 << "." \n");
    printf ( "You are going to replace value %d at position %d \n",Record->name,nIdx + 1);
    printf ( "Please type a new value: ");
    Node *Item = Record;
    for (int i=0;i<nIdx;i++)
-      {
-      Item=Item->pNext;
-      }
-   scanf("%s %d %d %d",&Item->name,
-                       &Item->num1,
+      Item = Item->pNext;
+   scanf("%s %c %d %le",&Item->name,
+                       &Item->symb,
                        &Item->num2,
                        &Item->num3);
 }
-
-k,bool saveAsBinary()    //Функция сохраняет список в бинарный файл
+//-------------------------------------------------------------------
+bool saveAsBinary()    //Функция сохраняет список в бинарный файл
 {
-   FILE *fp = fopen ("lab14.bin", "w");
+	/*r  Open for reading only.
+w  Create for writing. .
+a  Append; open for writing at end-of-file or create for writing if the file does not exist.
+r+  Open an existing file for update (reading and writing).
+w+  Create a new file for update (reading and writing).
+a+  Open for append; open (or create if the file does not exist) for update at the end of the file.
+
+To specify that a given file is being opened or created in text mode append a t to the mode string (rt w+t and so on); similarly to specify binary mode append a b to the mode string (wb a+b and so on).
+	*/
+
+	//FILE *fp = fopen ("lab14.bin", "w"); //К СИМВОЛЫУ 10 добавиться ЕЩЕ БАЙТ, СИВОЛ 13 не записывается!!!!
+	FILE *fp = fopen ("lab14.bin", "wb");
 
 	if (!fp)
-	m;jjj{
+	{
 		printf ( "Cannot create binary file");
 		return false;
 	}
 	fwrite(&nRecord, sizeof(int), 1, fp);
-        for (int i = 0; i < nRecord; ++i)
+   for (int i = 0; i < nRecord; ++i)
 	{
-           int len = strlen(Record->name);
-           fwrite(&len, sizeof(int), 1, fp);
-           fwrite(Record->name, 1, len, fp);  
-           fwrite(&Record->num1, sizeof(int), 1, fp);
-           fwrite(&Record->num2, sizeof(int), 1, fp);
-           fwrite(&Record->num3, sizeof(int), 1, fp);
-         //fwrite(&Record->pNext, sizeof(*Node), 1, fp);  Указатели сохранять не нужно
-         //fwrite(&Record->pPrev, sizeof(*Node), 1, fp);  Имхо проще создать новые
-           Record=Record->pNext;
+      int len = strlen(Record->name);
+      fwrite(&len, sizeof(int), 1, fp);
+      fwrite(Record->name, 1, len, fp);
+      fwrite(&Record->symb, sizeof(char), 1, fp);
+      fwrite(&Record->num2, sizeof(int), 1, fp);
+      fwrite(&Record->num3, sizeof(double), 1, fp);
+      //fwrite(&Record->pNext, sizeof(*Node), 1, fp);  Указатели сохранять не нужно
+      //fwrite(&Record->pPrev, sizeof(*Node), 1, fp);  Имхо проще создать новые
+      Record=Record->pNext;
 	}
 
 	fclose(fp);
 	return true;
 }
-
+//-------------------------------------------------------------------
 bool loadFromBinary()    //Функция загружает список из бинарного файла
 {
-   FILE *fp = fopen ("lab14.bin", "r");
+   FILE *fp = fopen ("lab14.bin", "rb");
 
 	if (!fp)
 	{
@@ -337,57 +358,52 @@ bool loadFromBinary()    //Функция загружает список из бинарного файла
 	fread(&nRecord, sizeof(int), 1, fp);
 	for (int i = 0; i < nRecord; ++i)
 	{
-           char name[80];
-           int num1;
-           int num2;
-           int num3; 
-           int len = 0;
-           fread(&len, sizeof(int), 1, fp);
-           fread(name, sizeof(char), len, fp);
-           name[len]='\0'; 
-           fread(&num1, sizeof(int), 1, fp);
-           fread(&num2, sizeof(int), 1, fp);
-           fread(&num3, sizeof(int), 1, fp);
-           //fread(&Record,sizeof(Record), 1, fp);
-           Node *Item = PushBack(Record,CreateItem(name,num1,num2,num3));
-           if(Record == NULL)
-           Record=Item;  
+      char name[80];
+      char symb;
+      int num2;
+      double num3;
+      int len = 0;
+      fread(&len, sizeof(int), 1, fp);
+      fread(name, sizeof(char), len, fp);
+      name[len]='\0';
+      fread(&symb, sizeof(char), 1, fp);
+      fread(&num2, sizeof(int), 1, fp);
+      fread(&num3, sizeof(double), 1, fp);
+      //fread(&Record,sizeof(Record), 1, fp);
+      Node *Item = PushBack(Record,CreateItem(name,symb,num2,num3));
+      if(Record == NULL)
+      Record=Item;
 	}
 
 	fclose(fp);
 	return true;
  }
+//-------------------------------------------------------------------
 bool saveAsText()   //Функция сохраняет список в текстовый файл
-{                                     
-   FILE *fp = fopen ("lab14.txt", "w");
-
+{
+	FILE *fp = fopen ("lab14.txt", "wt"); //
 	if (!fp)
 	{
 		printf ( "Cannot create text file");
 		return false;
 	}
-        //Node *Item = Record;
 
-	fprintf(fp, "%d ", nRecord);
+	fprintf(fp, "%d\n", nRecord);
 	for (int i = 0; i < nRecord; ++i)
 	{
-                fprintf(fp, "%s ", Record->name);
-                fprintf(fp, "%d ", Record->num1);
-                fprintf(fp, "%d ", Record->num2);
-                fprintf(fp, "%d ", Record->num3);
-              //fprintf(fp, "%p ", Record->pNext);
-              //fprintf(fp, "%p ", Record->pPrev);
-                Record=Record->pNext;
+       fprintf(fp, "%s ", Record->name); //А С ПРОБЕЛАМИ ТО КАСЯК
+       fprintf(fp, "%c ", Record->symb);
+       fprintf(fp, "%d ", Record->num2);
+       fprintf(fp, "%lf\n", Record->num3);
+       Record = Record->pNext;
 	} //while (Record->pNext != NULL);
 	fclose(fp);
-
 	return true;
 }
-
+//-------------------------------------------------------------------
 bool loadFromText()     //Функция загружает список из текстового файла
 {
-   FILE *fp = fopen ("lab14.txt", "r");
-
+	FILE *fp = fopen ("lab14.txt", "rt");
 	if (!fp)
 	{
 		printf ( "Cannot open text file");
@@ -400,35 +416,31 @@ bool loadFromText()     //Функция загружает список из текстового файла
 		Record = NULL;
 	}
 
-	fscanf(fp, "%d", &nRecord);
-
-                                            
+	fscanf(fp, "%d\n", &nRecord);
 	for (int i = 0; i < nRecord; ++i)
 	{
-           char name[80];
-           int num1;
-           int num2;
-           int num3;
-           fscanf(fp, "%s", &name);
-           fscanf(fp, "%d", &num1);
-           fscanf(fp, "%d", &num2);
-           fscanf(fp, "%d", &num3);
-         //fscanf(fp, "%d", &pNext);
-         //fscanf(fp, "%d", &pPrev);
-           Node *Item = PushBack(Record,CreateItem(name,num1,num2,num3));
-           if(Record == NULL)
-              Record=Item;
-              //Record=Record->pNext;
-	}  
+      char name[80];
+      char symb;
+      int num2;
+      double num3;
+      fscanf(fp, "%s ", &name);
+      fscanf(fp, "%c ", &symb);
+      fscanf(fp, "%d ", &num2);
+      fscanf(fp, "%lf\n", &num3);
+      Node *Item = PushBack(Record, CreateItem(name,symb,num2,num3));
+      if(Record == NULL)
+        Record=Item;
+        //Record=Record->pNext;
+	}
 	fclose(fp);
 	return true;
  }
-
+//-------------------------------------------------------------------
 bool saveReport()   //Функция сохраняет отсчет
 {
 	FILE *fp;
-	fp=fopen("report.txt","w");
-	if(fp==NULL)
+	fp=fopen("report.txt","wt");
+	if (fp == NULL)
 	{
 		printf("\nОшибка открытия файла...\n");
 		return false;
@@ -437,13 +449,70 @@ bool saveReport()   //Функция сохраняет отсчет
 	fprintf(fp,"+----+--------------+--------------------------+---------------------------+-----------------------------------+\n");
 	fprintf(fp,"| №  | Наименование | Минимальный рост в холке | Максимальный рост в холке |  Средняя продолжительность жизни  |\n");
 	fprintf(fp,"+----+--------------+--------------------------+---------------------------+-----------------------------------+\n");
-	for(int i=0;i<nRecord;++i)
+	for(int i=0; i<nRecord; ++i)
 	{
-		fprintf(fp,"| %-3d | %-20s | %-20d | %-20d | %-20d |\n",i+1,Record->name,Record->num1,Record->num2,Record->num3);
-                Record=Record->pNext;
+		fprintf(fp,"| %d | %s | %c | %d | %f |\n",
+		//fprintf(fp,"| %-2d | %-20s | %-c | %-20d | %-20f |\n",  ТАК НЕ ПАШЕТ
+         i+1, Record->name, Record->symb, Record->num2, Record->num3);
+      Record=Record->pNext; //ЗАПИСЫВАЕТ КАЛЯКУМАЛЯКУ
 	}
 	fprintf(fp,"+----+--------------+--------------------------+---------------------------+-----------------------------------+\n");
 	fclose(fp);
-        return true;
+   return true;
 
 }
+//-------------------------------------------------------------------
+bool ModifyBinary()
+{
+	FILE *fp = fopen ("BIN.bin", "r+b");
+	if (!fp)
+		{  printf ("Cannot open binary file");		return false;	}
+	if (Record)
+	{
+		delete [] Record;
+		Record = NULL;
+	}
+	int mas[8];
+	int WR = 55;
+	//long rew = ;
+	fread(&mas[0], sizeof(int), 1, fp);
+	fread(&mas[1], sizeof(int), 1, fp);
+	fread(&mas[2], sizeof(int), 1, fp);
+	fseek(fp, -(sizeof(int)), SEEK_CUR);
+	//fflush(fp);
+	int flush = mas[2]*2;
+	fwrite(&flush, sizeof(int), 1, fp);
+	//fflush(fp); //без этойхренине заптшется Нет запишется
+	fseek(fp, 0, SEEK_CUR);   //..безэтойхрени не  сработанет чтение и не щапишется
+	fread(&mas[3], sizeof(int), 1, fp);
+	fread(&mas[4], sizeof(int), 1, fp);
+	fread(&mas[5], sizeof(int), 1, fp);
+	fread(&mas[6], sizeof(int), 1, fp);
+	//fread(&mas[7], sizeof(int), 1, fp);
+	for (int i = 0; i < 8; ++i)
+		printf("[%d] = %d\n",i, mas[i]);
+	/*
+	fread(&nRecord, sizeof(int), 1, fp);
+	for (int i = 0; i < nRecord; ++i)
+	{
+      char name[80];
+      char symb;
+      int num2;
+      double num3;
+      int len = 0;
+      fread(&len, sizeof(int), 1, fp);
+      fread(name, sizeof(char), len, fp);
+      name[len]='\0';
+      fread(&symb, sizeof(char), 1, fp);
+		fread(&num2, sizeof(int), 1, fp);
+      fread(&num3, sizeof(double), 1, fp);
+      //fread(&Record,sizeof(Record), 1, fp);
+      Node *Item = PushBack(Record,CreateItem(name,symb,num2,num3));
+      if(Record == NULL)
+      Record=Item;
+	}            */
+
+	fclose(fp);
+	return true;
+}
+//-------------------------------------------------------------------
